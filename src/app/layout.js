a321139bb506c74f32a800c0c338574fd340dfe0
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-10-13 20:54:12 +0800

    fix: import missing files + update styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,26 @@
-"use client"
-
-import { Inter } from "next/font/google";
+"use client"; 
 import { Providers } from "./Providers";
-import { usePathname } from 'next/navigation'
-
-
+import "./globals.css";
 import Footer from "../components/footer";
+import { usePathname } from 'next/navigation'
 import Navbar from "@/components/navbar";
 import MainNavbar from "@/components/mainNavbar";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-
-  const currentPath = usePathname()
+    const currentPath = usePathname()
     console.log(currentPath)
 
   // Routes where no Navbar should be displayed 
-  const noNavbarRoutes = ['/']; // Homepage route
+  const noNavbarRoutes = ['/','/landing']; // Homepage route
 
   // Routes where MainNavbar (NewNavbar) should be displayed
   const mainNavbarRoutes = ['/login', '/register'];
 
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
       <body>
-      <Providers>
+        <Providers>
           {!noNavbarRoutes.includes(currentPath) && !mainNavbarRoutes.includes(currentPath) && <MainNavbar />}
           {mainNavbarRoutes.includes(currentPath) && <Navbar />}
           {children}
